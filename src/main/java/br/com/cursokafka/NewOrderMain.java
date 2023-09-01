2f5dcf3bb7f542f14d2394f409df902739ac249e
MarcoAurelioArJes <marcoaureliodea.jesus@gmail.com>
2023-09-01 12:50:07 -0300

    Consumidor de Email e Log criados

--- src/main/java/br/com/cursokafka/NewOrderMain.java
+++ src/main/java/br/com/cursokafka/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.cursokafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,14 +14,19 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(getProperties());
         var value = "543,050,1000";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("Sucesso enviado! Tópico: " + data.topic() + " ::: partition " + data.partition()
-                               + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+                    + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        producer.send(record, callback).get();
+
+        var email = "Thank you for you order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties getProperties() {
